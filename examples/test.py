397b95de3ecf37da6099fb815d07847cbd3d2018
brmmm3 <mrbm74@gmail.com>
2019-12-29 08:26:20 +0100

    Add simple test.py example

---
+++ examples/test.py
@@ -0,0 +1,36 @@
+
+import sys
+import time
+
+import scandir_rs as r
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: test.py <DirName>")
+        sys.exit(1)
+    root = sys.argv[1]
+
+    print("Get statistics...")
+    t1 = time.time()
+    c = r.count(root, metadata_ext=True)
+    dt = time.time() - t1
+    print(c)
+    print("dt =", dt)
+
+    print("Get TOC...")
+    t1 = time.time()
+    toc = r.toc(root)
+    dt = time.time() - t1
+    print([(key, len(value)) for key, value in toc.items()])
+    for key, value in sorted(toc.items()):
+        print(key, value[:3])
+    print("dt =", dt)
+
+    print("Get detailed list...")
+    t1 = time.time()
+    lst = r.list(root)
+    dt = time.time() - t1
+    print(len(lst))
+    print(lst[:3], "...")
+    print("dt =", dt)

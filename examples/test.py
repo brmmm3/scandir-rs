843d1adeab34c55873846c469ad037e43ee8a2d0
brmmm3 <mrbm74@gmail.com>
2019-12-31 07:56:39 +0100

    Improve example script test.py

--- examples/test.py
+++ examples/test.py
@@ -1,8 +1,29 @@
 
 import sys
 import time
+from threading import Thread
 
 import scandir_rs as r
+
+
+class Counter(Thread):
+
+    def __init__(self):
+        super().__init__(name="Counter", daemon=True)
+        self._cnt = 0
+        self._bRun = True
+
+    @property
+    def cnt(self):
+        return self._cnt
+
+    def stop(self):
+        self._bRun = False
+
+    def run(self):
+        while self._bRun:
+            self._cnt += 1
+            time.sleep(0.01)
 
 
 if __name__ == "__main__":
@@ -13,27 +34,36 @@ if __name__ == "__main__":
 
     print("Get statistics...")
     t1 = time.time()
+    thr = Counter()
+    thr.start()
     c = r.count(root, metadata_ext=True)
+    thr.stop()
     dt = time.time() - t1
     print(c)
-    print("dt =", dt)
+    print(f"dt={dt:.3f}  cnt={thr.cnt}")
 
     print("\nGet TOC...")
     t1 = time.time()
+    thr = Counter()
+    thr.start()
     toc = r.toc(root)
+    thr.stop()
     dt = time.time() - t1
     print("KEY -> CNT:", [(key, len(value)) for key, value in toc.items()])
     for key, value in sorted(toc.items()):
         print("KEY -> 3 VALUES:", key, value[:3])
-    print("dt =", dt)
+    print(f"dt={dt:.3f}  cnt={thr.cnt}")
 
     print("\nGet detailed list...")
     t1 = time.time()
+    thr = Counter()
+    thr.start()
     lst = r.list(root, metadata_ext=True)
+    thr.stop()
     dt = time.time() - t1
     print("CNT:", len(lst))
     for nr, (key, value) in enumerate(lst.items()):
         print(f"KEY -> VALUE: {key}: {value}")
         if nr > 2:
             break
-    print("dt =", dt)
+    print(f"dt={dt:.3f}  cnt={thr.cnt}")

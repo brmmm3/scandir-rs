8c9fdf5d6101910a6e994d6f8518ad106e6ca46f
brmmm3 <mrbm74@gmail.com>
2020-01-05 21:55:45 +0100

    Add option max_depth to submodule count.

--- src/count.rs
+++ src/count.rs
@@ -34,9 +34,10 @@ impl pyo3::class::PyObjectProtocol for Statistics {
 
 fn counter(
     root_path: String,
-    skip_hidden: Option<bool>,
-    metadata: Option<bool>,
-    metadata_ext: Option<bool>,
+    skip_hidden: bool,
+    metadata: bool,
+    metadata_ext: bool,
+    max_depth: usize,
     statistics: Arc<Mutex<Statistics>>,
     alive: Option<Arc<AtomicBool>>,
 ) {
@@ -55,10 +56,11 @@ fn counter(
     let mut pipes: u32 = 0;
     let mut cnt: u32 = 0;
     for entry in WalkDir::new(root_path)
-        .skip_hidden(skip_hidden.unwrap_or(false))
+        .skip_hidden(skip_hidden)
         .sort(false)
-        .preload_metadata(metadata.unwrap_or(false))
-        .preload_metadata_ext(metadata_ext.unwrap_or(false))
+        .preload_metadata(metadata)
+        .preload_metadata_ext(metadata_ext)
+        .max_depth(max_depth)
     {
         match &entry {
             Ok(v) => {
@@ -157,6 +159,7 @@ pub fn count(
     skip_hidden: Option<bool>,
     metadata: Option<bool>,
     metadata_ext: Option<bool>,
+    max_depth: Option<usize>,
 ) -> PyResult<PyObject> {
     let statistics = Arc::new(Mutex::new(Statistics { 
         dirs: 0,
@@ -171,7 +174,12 @@ pub fn count(
     }));
     let stats = statistics.clone();
     let rc: std::result::Result<(), std::io::Error> = py.allow_threads(|| {
-        counter(root_path, skip_hidden, metadata, metadata_ext, stats, None);
+        counter(root_path,
+                skip_hidden.unwrap_or(false),
+                metadata.unwrap_or(false),
+                metadata_ext.unwrap_or(false),
+                max_depth.unwrap_or(::std::usize::MAX),
+                stats, None);
         Ok(())
     });
     let pyresult = PyDict::new(py);
@@ -215,9 +223,10 @@ pub fn count(
 pub struct Count {
     // Options
     root_path: String,
-    skip_hidden: Option<bool>,
-    metadata: Option<bool>,
-    metadata_ext: Option<bool>,
+    skip_hidden: bool,
+    metadata: bool,
+    metadata_ext: bool,
+    max_depth: usize,
     // Results
     statistics: Arc<Mutex<Statistics>>,
     // Internal
@@ -235,12 +244,14 @@ impl Count {
         skip_hidden: Option<bool>,
         metadata: Option<bool>,
         metadata_ext: Option<bool>,
+        max_depth: Option<usize>,
     ) {
         obj.init(Count {
             root_path: String::from(root_path),
-            skip_hidden: skip_hidden,
-            metadata: metadata,
-            metadata_ext: metadata_ext,
+            skip_hidden: skip_hidden.unwrap_or(false),
+            metadata: metadata.unwrap_or(false),
+            metadata_ext: metadata_ext.unwrap_or(false),
+            max_depth: max_depth.unwrap_or(::std::usize::MAX),
             statistics: Arc::new(Mutex::new(Statistics { 
                 dirs: 0,
                 files: 0,
@@ -264,7 +275,8 @@ impl Count {
     }
 
     fn collect(&self) -> PyResult<Statistics> {
-        counter(self.root_path.clone(), self.skip_hidden, self.metadata, self.metadata_ext,
+        counter(self.root_path.clone(),
+                self.skip_hidden, self.metadata, self.metadata_ext, self.max_depth,
                 self.statistics.clone(), None);
         Ok(Arc::clone(&self.statistics).lock().unwrap().clone())
     }
@@ -277,11 +289,14 @@ impl Count {
         let skip_hidden = self.skip_hidden;
         let metadata = self.metadata;
         let metadata_ext = self.metadata_ext;
+        let max_depth = self.max_depth;
         let statistics = self.statistics.clone();
         let alive = Arc::new(AtomicBool::new(true));
         self.alive = Some(Arc::downgrade(&alive));
         self.thr = Some(thread::spawn(move || {
-            counter(root_path, skip_hidden, metadata, metadata_ext, statistics, Some(alive))
+            counter(root_path,
+                    skip_hidden, metadata, metadata_ext, max_depth,
+                    statistics, Some(alive))
         }));
         Ok(true)
     }
@@ -326,11 +341,13 @@ impl<'p> PyContextProtocol<'p> for Count {
         let skip_hidden = self.skip_hidden;
         let metadata = self.metadata;
         let metadata_ext = self.metadata_ext;
+        let max_depth = self.max_depth;
         let statistics = self.statistics.clone();
         let alive = Arc::new(AtomicBool::new(true));
         self.alive = Some(Arc::downgrade(&alive));
         self.thr = Some(thread::spawn(move || {
-            counter(root_path, skip_hidden, metadata, metadata_ext,
+            counter(root_path,
+                    skip_hidden, metadata, metadata_ext, max_depth,
                     statistics, Some(alive))
         }));
         Ok(())

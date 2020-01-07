3d202d2571d108a8d01726f19d047d3e1011d958
Martin Bammer <mrbm74@gmail.com>
2020-01-07 22:15:34 +0100

    Improve and optimize submodule scandir

--- src/scandir.rs
+++ src/scandir.rs
@@ -1,6 +1,4 @@
-use std::time::UNIX_EPOCH;
-use std::collections::HashMap;
-use std::time::{Instant, Duration};
+use std::time::{UNIX_EPOCH, Instant};
 use std::thread;
 use std::sync::{Arc, Mutex, Weak};
 use std::sync::atomic::{AtomicBool, Ordering};
@@ -8,25 +6,161 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use jwalk::WalkDir;
 #[cfg(unix)]
 use expanduser::expanduser;
+use crossbeam_channel as channel;
 
 use pyo3::prelude::*;
-use pyo3::types::{PyType, PyAny, PyDict};
+use pyo3::types::{PyType, PyAny, PyTuple, PyDict};
 use pyo3::{Python, wrap_pyfunction, PyContextProtocol, PyIterProtocol};
 use pyo3::exceptions::{self, ValueError};
 
 use crate::def::*;
 
+#[derive(Debug, Clone)]
+pub enum Stats {
+    DirEntry(DirEntry),
+    Error(String),
+    Duration(f64),
+}
+
+#[derive(Debug, Clone)]
+pub struct Entry {
+    path: String,
+    entry: Stats,
+}
+
+impl ToPyObject for Entry {
+    #[inline]
+    fn to_object(&self, py: Python) -> PyObject {
+        match &self.entry {
+            Stats::DirEntry(e) => PyTuple::new(py, &[self.path.to_object(py), PyRef::new(py, e.clone()).unwrap().to_object(py)]).into(),
+            Stats::Error(e) => PyTuple::new(py, &[self.path.to_object(py), e.to_object(py)]).into(),
+            Stats::Duration(e) => PyTuple::new(py, &[self.path.to_object(py), e.to_object(py)]).into(),
+        }
+    }
+}
+
 #[pyclass]
 #[derive(Debug, Clone)]
 pub struct Entries {
-    pub entries: HashMap<String, DirEntry>,
-    pub errors: Vec<String>,
+    pub entries: Vec<Entry>,
+    pub duration: f64,
 }
 
 #[pyproto]
 impl pyo3::class::PyObjectProtocol for Entries {
     fn __str__(&self) -> PyResult<String> {
         Ok(format!("{:?}", self))
+    }
+}
+
+#[pymethods]
+impl Entries {
+    #[getter]
+    fn entries(&self) -> PyResult<PyObject> {
+        Ok(PyTuple::new(GILGuard::acquire().python(), &self.entries).into())
+    }
+
+    #[getter]
+    fn duration(&self) -> PyResult<f64> {
+       Ok(self.duration)
+    }
+}
+
+fn create_entry(entry: &std::result::Result<jwalk::core::dir_entry::DirEntry<()>, std::io::Error>) -> Entry {
+    match entry {
+        Ok(v) => {
+            let file_type = v.file_type_result.as_ref().unwrap();
+            let mut ctime: f64 = 0.0;
+            let mut mtime: f64 = 0.0;
+            let mut atime: f64 = 0.0;
+            let mut mode: u32 = 0;
+            let mut ino: u64 = 0;
+            let mut dev: u64 = 0;
+            let mut nlink: u64 = 0;
+            let mut size: u64 = 0;
+            let mut blksize: u64 = 4096;
+            let mut blocks: u64 = 0;
+            #[cfg(unix)]
+            let mut uid: u32 = 0;
+            #[cfg(unix)]
+            let mut gid: u32 = 0;
+            #[cfg(unix)]
+            let mut rdev: u64 = 0;
+            if v.metadata_result.is_some() {
+                let metadata = v.metadata_result.as_ref().unwrap().as_ref().unwrap();
+                let duration = metadata
+                    .created()
+                    .unwrap()
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap();
+                ctime = duration.as_secs() as f64 + duration.subsec_nanos() as f64 * 1e-9;
+                let duration = metadata
+                    .modified()
+                    .unwrap()
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap();
+                mtime = duration.as_secs() as f64 + duration.subsec_nanos() as f64 * 1e-9;
+                let duration = metadata
+                    .accessed()
+                    .unwrap()
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap();
+                atime = duration.as_secs() as f64 + duration.subsec_nanos() as f64 * 1e-9;
+                let metadata_ext = v.ext.as_ref();
+                if metadata_ext.is_some() {
+                    let metadata_ext = metadata_ext.unwrap().as_ref().unwrap();
+                    mode = metadata_ext.mode;
+                    ino = metadata_ext.ino;
+                    dev = metadata_ext.dev as u64;
+                    nlink = metadata_ext.nlink as u64;
+                    size = metadata_ext.size;
+                    #[cfg(unix)]
+                    {
+                        blksize = metadata_ext.blksize;
+                        blocks = metadata_ext.blocks;
+                        uid = metadata_ext.uid;
+                        gid = metadata_ext.gid;
+                        rdev = metadata_ext.rdev;
+                    }
+                    #[cfg(windows)]
+                    {
+                        blksize = 4096;
+                        blocks = size >> 12;
+                        if blocks << 12 < size {
+                            blocks += 1;
+                        }
+                    }
+                }
+            }
+            let mut key = v.parent_path.to_path_buf();
+            key.push(v.file_name.clone().into_string().unwrap());
+            let entry = DirEntry {
+                    is_symlink: file_type.is_symlink(),
+                    is_dir: file_type.is_dir(),
+                    is_file: file_type.is_file(),
+                    ctime: ctime,
+                    mtime: mtime,
+                    atime: atime,
+                    mode: mode,
+                    ino: ino,
+                    dev: dev,
+                    nlink: nlink,
+                    size: size,
+                    blksize: blksize,
+                    blocks: blocks,
+                    #[cfg(unix)]
+                    uid: uid,
+                    #[cfg(unix)]
+                    gid: gid,
+                    #[cfg(unix)]
+                    rdev: rdev,
+                };
+            Entry{ path: key.to_str().unwrap().to_string(),
+                   entry: Stats::DirEntry(entry) }
+        },
+        // TODO: Need to fetch failed path from somewhere
+        Err(e) => Entry { path: String::from("?"),
+                          entry: Stats::Error(e.to_string()) }
     }
 }
 
@@ -42,6 +176,7 @@ fn rs_entries(
 ) {
     #[cfg(unix)]
     let root_path = expanduser(root_path).unwrap();
+    let start_time = Instant::now();
     for entry in WalkDir::new(root_path)
         .skip_hidden(skip_hidden)
         .sort(sorted)
@@ -49,105 +184,60 @@ fn rs_entries(
         .preload_metadata_ext(metadata_ext)
         .max_depth(max_depth)
     {
-        match &entry {
-            Ok(v) => {
-                let file_type = v.file_type_result.as_ref().unwrap();
-                let mut ctime: f64 = 0.0;
-                let mut mtime: f64 = 0.0;
-                let mut atime: f64 = 0.0;
-                let mut mode: u32 = 0;
-                let mut ino: u64 = 0;
-                let mut dev: u64 = 0;
-                let mut nlink: u64 = 0;
-                let mut size: u64 = 0;
-                let mut blksize: u64 = 4096;
-                let mut blocks: u64 = 0;
-                #[cfg(unix)]
-                let mut uid: u32 = 0;
-                #[cfg(unix)]
-                let mut gid: u32 = 0;
-                #[cfg(unix)]
-                let mut rdev: u64 = 0;
-                if v.metadata_result.is_some() {
-                    let metadata = v.metadata_result.as_ref().unwrap().as_ref().unwrap();
-                    let duration = metadata
-                        .created()
-                        .unwrap()
-                        .duration_since(UNIX_EPOCH)
-                        .unwrap();
-                    ctime = duration.as_secs() as f64 + duration.subsec_nanos() as f64 * 1e-9;
-                    let duration = metadata
-                        .modified()
-                        .unwrap()
-                        .duration_since(UNIX_EPOCH)
-                        .unwrap();
-                    mtime = duration.as_secs() as f64 + duration.subsec_nanos() as f64 * 1e-9;
-                    let duration = metadata
-                        .accessed()
-                        .unwrap()
-                        .duration_since(UNIX_EPOCH)
-                        .unwrap();
-                    atime = duration.as_secs() as f64 + duration.subsec_nanos() as f64 * 1e-9;
-                    let metadata_ext = v.ext.as_ref();
-                    if metadata_ext.is_some() {
-                        let metadata_ext = metadata_ext.unwrap().as_ref().unwrap();
-                        mode = metadata_ext.mode;
-                        ino = metadata_ext.ino;
-                        dev = metadata_ext.dev as u64;
-                        nlink = metadata_ext.nlink as u64;
-                        size = metadata_ext.size;
-                        #[cfg(unix)]
-                        {
-                            blksize = metadata_ext.blksize;
-                            blocks = metadata_ext.blocks;
-                            uid = metadata_ext.uid;
-                            gid = metadata_ext.gid;
-                            rdev = metadata_ext.rdev;
-                        }
-                        #[cfg(windows)]
-                        {
-                            blksize = 4096;
-                            blocks = size >> 12;
-                            if blocks << 12 < size {
-                                blocks += 1;
-                            }
-                        }
-                    }
-                }
-                let mut key = v.parent_path.to_path_buf();
-                key.push(v.file_name.clone().into_string().unwrap());
-                let entry = DirEntry {
-                        is_symlink: file_type.is_symlink(),
-                        is_dir: file_type.is_dir(),
-                        is_file: file_type.is_file(),
-                        ctime: ctime,
-                        mtime: mtime,
-                        atime: atime,
-                        mode: mode,
-                        ino: ino,
-                        dev: dev,
-                        nlink: nlink,
-                        size: size,
-                        blksize: blksize,
-                        blocks: blocks,
-                        #[cfg(unix)]
-                        uid: uid,
-                        #[cfg(unix)]
-                        gid: gid,
-                        #[cfg(unix)]
-                        rdev: rdev,
-                    };
-                let mut result_locked = result.lock().unwrap();
-                result_locked.entries.insert(key.to_str().unwrap().to_string(), entry);
-            }
-            Err(e) => result.lock().unwrap().errors.push(e.to_string())  // TODO: Need to fetch failed path from somewhere
-        };
+        let entry = create_entry(&entry);
+        let mut result_locked = result.lock().unwrap();
+        result_locked.entries.push(entry);
+        result_locked.duration = start_time.elapsed().as_millis() as f64 * 0.001;
         match &alive {
             Some(a) => if !a.load(Ordering::Relaxed) {
                 break;
             },
             None => {},
         }
+    }
+    result.lock().unwrap().duration = start_time.elapsed().as_millis() as f64 * 0.001;
+}
+
+fn rs_entries_iter(
+    root_path: String,
+    sorted: bool,
+    skip_hidden: bool,
+    metadata: bool,
+    metadata_ext: bool,
+    max_depth: usize,
+    alive: Option<Arc<AtomicBool>>,
+    tx: Option<channel::Sender<Entry>>,
+) {
+    #[cfg(unix)]
+    let root_path = expanduser(root_path).unwrap();
+    let start_time = Instant::now();
+    for entry in WalkDir::new(root_path)
+        .skip_hidden(skip_hidden)
+        .sort(sorted)
+        .preload_metadata(metadata)
+        .preload_metadata_ext(metadata_ext)
+        .max_depth(max_depth)
+    {
+        let entry = create_entry(&entry);
+        match &tx {
+            Some(tx) => {
+                tx.send(entry).unwrap();
+            },
+            None => {},
+        }
+        match &alive {
+            Some(a) => if !a.load(Ordering::Relaxed) {
+                break;
+            },
+            None => {},
+        }
+    }
+    match &tx {
+        Some(tx) => {
+            tx.send(Entry { path: String::from("?"),
+                            entry: Stats::Duration(start_time.elapsed().as_millis() as f64 * 0.001) }).unwrap();
+        },
+        None => {},
     }
 }
 
@@ -162,8 +252,8 @@ pub fn entries(
     max_depth: Option<usize>,
 ) -> PyResult<Entries> {
     let result = Arc::new(Mutex::new(Entries {
-        entries: HashMap::new(),
-        errors: Vec::new(),
+        entries: Vec::new(),
+        duration: 0.0,
     }));
     let result_cloned = result.clone();
     let rc: std::result::Result<(), std::io::Error> = py.allow_threads(|| {
@@ -199,23 +289,22 @@ pub struct Scandir {
     // Internal
     thr: Option<thread::JoinHandle<()>>,
     alive: Option<Weak<AtomicBool>>,
+    rx: Option<channel::Receiver<Entry>>,
     has_results: bool,
-    start_time: Instant,
-    duration: Duration,
 }
 
 impl Scandir {
     fn rs_init(&self) {
         let mut entries_locked = self.entries.lock().unwrap();
         entries_locked.entries.clear();
-        entries_locked.errors.clear();
     }
 
-    fn rs_start(&mut self) -> bool {
+    fn rs_start(&mut self,
+        tx: Option<channel::Sender<Entry>>,
+    ) -> bool {
         if self.thr.is_some() {
             return false
         }
-        self.start_time = Instant::now();
         if self.has_results {
             self.rs_init();
         }
@@ -229,9 +318,15 @@ impl Scandir {
         let alive = Arc::new(AtomicBool::new(true));
         self.alive = Some(Arc::downgrade(&alive));
         self.thr = Some(thread::spawn(move || {
-            rs_entries(root_path,
-                       sorted, skip_hidden, metadata, metadata_ext, max_depth,
-                       entries, Some(alive))
+            if tx.is_none() {
+                rs_entries(root_path,
+                        sorted, skip_hidden, metadata, metadata_ext, max_depth,
+                        entries, Some(alive))
+            } else {
+                rs_entries_iter(root_path,
+                    sorted, skip_hidden, metadata, metadata_ext, max_depth,
+                    Some(alive), tx)
+        }
         }));
         true
     }
@@ -245,7 +340,6 @@ impl Scandir {
             None => {},
         }
         self.thr.take().map(thread::JoinHandle::join);
-        self.duration = self.start_time.elapsed();
         self.has_results = true;
         true
     }
@@ -270,15 +364,14 @@ impl Scandir {
             metadata: metadata.unwrap_or(false),
             metadata_ext: metadata_ext.unwrap_or(false),
             max_depth: max_depth.unwrap_or(::std::usize::MAX),
-            entries: Arc::new(Mutex::new(Entries { 
-                entries: HashMap::new(),
-                errors: Vec::new(),
+            entries: Arc::new(Mutex::new(Entries {
+                entries: Vec::new(),
+                duration: 0.0,
             })),
             thr: None,
             alive: None,
+            rx: None,
             has_results: false,
-            start_time: Instant::now(),
-            duration: Duration::new(0, 0),
         });
     }
 
@@ -287,25 +380,21 @@ impl Scandir {
        Ok(Arc::clone(&self.entries).lock().unwrap().clone())
     }
 
-    #[getter]
-    fn duration(&self) -> PyResult<f64> {
-       Ok(self.duration.as_secs() as f64 + self.duration.subsec_nanos() as f64 * 1e-9)
-    }
-
     fn has_results(&self) -> PyResult<bool> {
         Ok(self.has_results)
      }
- 
+
      fn as_dict(&self) -> PyResult<PyObject> {
+        let entries_locked = self.entries.lock().unwrap();
         let gil = GILGuard::acquire();
         let py = gil.python();
-        let mut entries_locked = self.entries.lock().unwrap();
-        let pyresult = PyDict::new(gil.python());
-        for (k, v) in entries_locked.entries.drain() {
-            pyresult.set_item(k, PyRef::new(py, v).unwrap())?;
-        }
-        if !entries_locked.errors.is_empty() {
-            pyresult.set_item(py.None(), entries_locked.errors.to_vec())?;
+        let pyresult = PyDict::new(py);
+        for entry in &entries_locked.entries {
+            match &entry.entry {
+                Stats::DirEntry(e) => pyresult.set_item(entry.path.to_object(py), PyRef::new(py, e.clone()).unwrap()).unwrap(),
+                Stats::Error(e) => pyresult.set_item(entry.path.to_object(py), e).unwrap(),
+                Stats::Duration(_) => {},
+            }
         }
         Ok(pyresult.into())
     }
@@ -318,7 +407,7 @@ impl Scandir {
     }
 
     fn start(&mut self) -> PyResult<bool> {
-        if !self.rs_start() {
+        if !self.rs_start(None) {
             return Err(exceptions::RuntimeError::py_err("Thread already running"))
         }
         Ok(true)
@@ -352,7 +441,9 @@ impl pyo3::class::PyObjectProtocol for Scandir {
 #[pyproto]
 impl<'p> PyContextProtocol<'p> for Scandir {
     fn __enter__(&'p mut self) -> PyResult<()> {
-        self.rs_start();
+        if !self.rs_start(None) {
+            return Err(exceptions::RuntimeError::py_err("Thread already running"))
+        }
         Ok(())
     }
 
@@ -375,17 +466,34 @@ impl<'p> PyContextProtocol<'p> for Scandir {
 
 #[pyproto]
 impl<'p> PyIterProtocol for Scandir {
-    fn __iter__(slf: PyRefMut<Self>) -> PyResult<Py<Scandir>> {
+    fn __iter__(mut slf: PyRefMut<Self>) -> PyResult<Py<Scandir>> {
+        if slf.thr.is_some() {
+            return Err(exceptions::RuntimeError::py_err("Thread already running"))
+        }
+        let (tx, rx) = channel::unbounded();
+        slf.rx = Some(rx);
+        slf.rs_start(Some(tx));
         Ok(slf.into())
     }
 
-    fn __next__(slf: PyRefMut<Self>) -> PyResult<Option<Entries>> {
-        let entries_locked = slf.entries.lock().unwrap();
-        if entries_locked.entries.is_empty()
-                && entries_locked.errors.is_empty() {
-            return Ok(None)
+    fn __next__(slf: PyRefMut<Self>) -> PyResult<Option<PyObject>> {
+        let gil = GILGuard::acquire();
+        let py = gil.python();
+        match &slf.rx {
+            Some(rx) => match rx.recv() {
+                Ok(val) => match &val.entry {
+                    Stats::DirEntry(_) => Ok(Some(val.to_object(py))),
+                    Stats::Error(_) => Ok(Some(val.to_object(py))),
+                    Stats::Duration(d) => {
+                        let mut entries_locked = slf.entries.lock().unwrap();
+                        entries_locked.duration = *d;
+                        Ok(None)
+                    }
+                },
+                Err(_) => Ok(None),
+            },
+            None => Ok(None),
         }
-        Ok(Some(entries_locked.clone()))
     }
 }
 

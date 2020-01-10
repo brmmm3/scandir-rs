25646e565d41c1c7c14b5e028500ed2364d1a03b
Martin Bammer <mrbm74@gmail.com>
2020-01-10 16:37:55 +0100

    Fix tests.

--- src/test.rs
+++ src/test.rs
@@ -10,9 +10,9 @@ mod tests {
         let gil = Python::acquire_gil();
         let py = gil.python();
         #[cfg(unix)]
-        println!("{:#?}", count(py, String::from("/usr"), Some(false), Some(false), Some(false), None).unwrap());
+        println!("{:#?}", count(py, String::from("/usr"), Some(false), Some(false), None).unwrap());
         #[cfg(windows)]
-        println!("{:#?}", count(py, String::from("C:/Windows"), Some(false), Some(false), Some(false), None).unwrap());
+        println!("{:#?}", count(py, String::from("C:/Windows"), Some(false), Some(false), None).unwrap());
     }
 
     #[test]
@@ -21,9 +21,9 @@ mod tests {
         let py = gil.python();
 
         #[cfg(unix)]
-        count(py, String::from("/usr"), Some(true), Some(false), Some(false), None).unwrap();
+        count(py, String::from("/usr"), Some(true), Some(false), None).unwrap();
         #[cfg(windows)]
-        count(py, String::from("C:/Windows"), Some(true), Some(false), Some(false), None).unwrap();
+        count(py, String::from("C:/Windows"), Some(false), Some(false), None).unwrap();
     }
 
     #[test]
@@ -32,9 +32,9 @@ mod tests {
         let py = gil.python();
 
         #[cfg(unix)]
-        println!("{:#?}", count(py, String::from("/usr"), Some(true), Some(true), Some(false), None).unwrap());
+        println!("{:#?}", count(py, String::from("/usr"), Some(true), Some(false), None).unwrap());
         #[cfg(windows)]
-        println!("{:#?}", count(py, String::from("C:/Windows"), Some(true), Some(true), Some(false), None).unwrap());
+        println!("{:#?}", count(py, String::from("C:/Windows"), Some(true), Some(false), None).unwrap());
     }
 
     #[test]
@@ -43,8 +43,8 @@ mod tests {
         let py = gil.python();
 
         #[cfg(unix)]
-        count(py, String::from("/usr"), Some(true), Some(true), Some(true), None).unwrap();
+        count(py, String::from("/usr"), Some(true), Some(true), None).unwrap();
         #[cfg(windows)]
-        count(py, String::from("C:/Windows"), Some(true), Some(true), Some(true), None).unwrap();
+        count(py, String::from("C:/Windows"), Some(true), Some(true), None).unwrap();
     }
 }

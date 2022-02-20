8f91c3f9401cf1b65c53dd3d5b9bc95ce96ee4b2
brmmm3 <mrbm74@gmail.com>
2022-02-20 09:13:14 +0100

    Add build script for just creating Python 3.10 wheel

---
+++ build_wheel_3.10.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+eval "$(pyenv init -)"
+name=`grep -Po '\bname\s*=\s*"\K.*?(?=")' Cargo.toml | head -1 | tr - _`
+version=`grep -Po '\bversion\s*=\s*"\K.*?(?=")' Cargo.toml | head -1`
+
+pyenv shell 3.10.2
+pip install --upgrade pip
+pip install -U pytest
+maturin build --release --strip -i python3.10
+pip install --force-reinstall build/wheels/$name-$version-cp310-cp310-manylinux1_x86_64.whl
+python3.10 -m pytest
+
+pyenv shell --unset

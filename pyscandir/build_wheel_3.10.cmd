c031a2c34509c720bcead82129b53f8c11d83384
brmmm3 <mrbm74@gmail.com>
2022-05-05 21:43:43 +0200

    Update changelog.

--- pyscandir/build_wheel_3.10.cmd
+++ pyscandir/build_wheel_3.10.cmd
@@ -2,5 +2,5 @@ pyenv shell 3.10.4
 pip install --upgrade pip
 pip install -U pytest
 maturin build --release --strip -i python3.10
-pip install --force-reinstall ../target/wheels/scandir_rs-2.0.2-cp310-none-win_amd64.whl
+pip install --force-reinstall ../target/wheels/scandir_rs-2.0.3-cp310-none-win_amd64.whl
 python3.10 -m pytest

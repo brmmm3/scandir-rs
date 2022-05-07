a43e9eb2a68f7d3e41cc830ff7eba943d87aa8af
brmmm3 <mrbm74@gmail.com>
2022-05-07 07:47:11 +0200

    Fix build script

--- pyscandir/build_wheel_3.10.sh
+++ pyscandir/build_wheel_3.10.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 eval "$(pyenv init -)"
 
-name=`grep -Po '\bname\s*=\s*"\K.*?(?=")' Cargo.toml | head -1 | tr - _`
+name=`grep -Po '\bname\s*=\s*"\K.*?(?=")' Cargo.toml | tail -1 | tr - _`
 version=`grep -Po '\bversion\s*=\s*"\K.*?(?=")' Cargo.toml | head -1`
 
 pyenv shell 3.10.4
 pip install --upgrade pip
 pip install -U pytest
-maturin build --release --strip -i python3.10
+maturin build --release --strip
 pip install --force-reinstall ../target/wheels/$name-$version-cp310-cp310-linux_x86_64.whl
-python3.10 -m pytest
+python -m pytest

ee4b908b45780ca54e26aeaa5de0e668c85b0099
Martin Bammer <mrbm74@gmail.com>
2023-02-13 12:27:43 +0100

    Update dependencies

--- pyscandir/examples/count.py
+++ pyscandir/examples/count.py
@@ -12,9 +12,3 @@ print(Count(dirName).collect())
 # Statistics { dirs: 76923, files: 648585, slinks: 48089,
 #              hlinks: 0, devices: 0, pipes: 0, size: 0, usage: 0,
 #              errors: [], duration: 1.07 }
-
-print(Count(dirName, extended=True).collect())
-# Output is something like:
-# Statistics { dirs: 76923, files: 648585, slinks: 48089,
-#              hlinks: 1113, devices: 0, pipes: 0, size: 32448804258,
-#              usage: 34060193792, errors: [], duration: 0.934 }

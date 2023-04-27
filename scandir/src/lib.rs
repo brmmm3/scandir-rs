4d2517b1d611cd7c70ac4a9b76077b160340b454
Martin Bammer <mrbm74@gmail.com>
2023-04-27 08:23:21 +0200

    Add some documentation

--- scandir/src/lib.rs
+++ scandir/src/lib.rs
@@ -1,3 +1,14 @@
+//! `scandir` is a directory iteration module like `walk`, but with more features and higher speed. Depending on the function call
+//! it yields a list of paths, tuple of lists grouped by their entry type or ``DirEntry`` objects that include file type and stat information along
+//! with the name.
+//!
+//! If you are just interested in directory statistics you can use the ``Count``.
+//!
+//! `scandir` contains following classes:
+//! - `Count` for determining statistics of a directory.
+//! - `Walk` for getting names of directory entries.
+//! - `Scandir` for getting detailed stats of directory entries.
+
 #![cfg_attr(windows, feature(windows_by_handle))]
 
 extern crate glob_sl;

46423876b7f04728f40243937798ecd34aff10d3
Martin Bammer <mrbm74@gmail.com>
2024-04-10 08:11:44 +0200

    Update benchmark.py

--- pyscandir/examples/benchmark.py
+++ pyscandir/examples/benchmark.py
@@ -2,6 +2,11 @@
 
 import os
 import time
+import platform
+
+import psutil
+from diskinfo import DiskInfo
+from tabulate import tabulate
 
 from scandir_rs import Count, Walk, Scandir, ReturnType
 
@@ -18,6 +23,20 @@ def scantree(path):
         return
 
 
+def GetDiskInfo():
+    partition = [
+        p for p in psutil.disk_partitions(all=False) if p.mountpoint in ("/", "C:\\")
+    ][0]
+    disks = DiskInfo().get_disk_list()
+    for disk in disks:
+        if partition.device.startswith(disk.get_path()):
+            return (
+                disk.get_model(),
+                ("SSD" if disk.is_ssd() else "NVME" if disk.is_nvme() else "HDD"),
+                partition.fstype,
+            )
+
+
 if os.name == "nt":
     dirName = "C:/Windows"
 else:
@@ -30,12 +49,55 @@ print(Count(dirName).collect())
 print(Count(dirName, return_type=ReturnType.Ext).collect())
 print()
 
+table = []
+
+t1 = time.time()
+toc = Count(dirName).collect()
+dt = time.time() - t1
+print(f"Count.collect: {dt:.3f}")
+table.append([f"{dt:.3}", "Count.collect"])
+
+t1 = time.time()
+toc = Count(dirName, return_type=ReturnType.Ext).collect()
+dt = time.time() - t1
+print(f"Count(ReturnType=Ext).collect: {dt:.3f}")
+table.append([f"{dt:.3}", "Count(ReturnType=Ext).collect"])
+
 t1 = time.time()
 cnt = 0
 for root, dirs, files in os.walk(os.path.expanduser(dirName)):
     cnt += 1
 dt = time.time() - t1
 print(f"os.walk: {dt:.3f} {cnt}")
+table.append([f"{dt:.3}", "os.walk"])
+
+t1 = time.time()
+cnt = 0
+for result in Walk(dirName):
+    cnt += 1
+dt = time.time() - t1
+print(f"Walk.iter: {dt:.3f} {cnt}")
+table.append([f"{dt:.3}", "Walk.iter"])
+
+t1 = time.time()
+toc = Walk(dirName).collect()
+dt = time.time() - t1
+print(f"Walk.collect: {dt:.3f} dirs={len(toc.dirs)} files=={len(toc.files)}")
+table.append([f"{dt:.3}", "Walk.collect"])
+
+t1 = time.time()
+instance = Walk(dirName)
+toc = instance.collect()
+dt = time.time() - t1
+print(
+    f"Walk.collect: {dt:.3f} dirs={len(toc.dirs)} files=={len(toc.files)} Walk().duration={instance.duration()}"
+)
+
+t1 = time.time()
+toc = Walk(dirName, return_type=ReturnType.Ext).collect()
+dt = time.time() - t1
+print(f"Walk(ReturnType=Ext).collect: {dt:.3f} {str(toc)[:500]}")
+table.append([f"{dt:.3}", "Walk(ReturnType=Ext).collect"])
 
 t1 = time.time()
 dirs = 0
@@ -55,47 +117,14 @@ for entry in scantree(os.path.expanduser(dirName)):
         symlinks += 1
     size += st.st_size
 dt = time.time() - t1
-print(f"scantree: {dt:.3f} {dirs=} {files=} {symlinks=} {size=}")
-
-t1 = time.time()
-toc = Count(dirName).collect()
-dt = time.time() - t1
-print(f"Count.collect: {dt:.3f}")
-
-t1 = time.time()
-toc = Count(dirName, return_type=ReturnType.Ext).collect()
-dt = time.time() - t1
-print(f"Count(ReturnType=Ext).collect: {dt:.3f}")
-
-t1 = time.time()
-cnt = 0
-for result in Walk(dirName):
-    cnt += 1
-dt = time.time() - t1
-print(f"Walk.iter: {dt:.3f} {cnt}")
-
-t1 = time.time()
-toc = Walk(dirName).collect()
-dt = time.time() - t1
-print(f"Walk.collect: {dt:.3f} dirs={len(toc.dirs)} files=={len(toc.files)}")
-
-t1 = time.time()
-instance = Walk(dirName)
-toc = instance.collect()
-dt = time.time() - t1
-print(
-    f"Walk.collect: {dt:.3f} dirs={len(toc.dirs)} files=={len(toc.files)} Walk().duration={instance.duration()}"
-)
-
-t1 = time.time()
-toc = Walk(dirName, return_type=ReturnType.Ext).collect()
-dt = time.time() - t1
-print(f"Walk(ReturnType=Ext).collect: {dt:.3f} {str(toc)[:500]}")
+print(f"scantree (os.scandir): {dt:.3f} {dirs=} {files=} {symlinks=} {size=}")
+table.append([f"{dt:.3}", "scantree (os.scandir)"])
 
 t1 = time.time()
 entries = Scandir(dirName).collect()
 dt = time.time() - t1
 print(f"Scandir.collect: {dt:.3f} {len(entries)}")
+table.append([f"{dt:.3}", "Scandir.collect"])
 
 t1 = time.time()
 instance = Scandir(dirName)
@@ -104,6 +133,7 @@ for entry in instance:
     cnt += 1
 dt = time.time() - t1
 print(f"Scandir.iter: {dt:.3f} {cnt}")
+table.append([f"{dt:.3}", "Scandir.iter"])
 
 t1 = time.time()
 instance = Scandir(dirName)
@@ -115,3 +145,15 @@ t1 = time.time()
 entries = Scandir(dirName, return_type=ReturnType.Ext).collect()
 dt = time.time() - t1
 print(f"Scandir(ReturnType=Ext).collect: {dt:.3f} {len(entries)}")
+table.append([f"{dt:.3}", "Scandir(ReturnType=Ext).collect"])
+
+uname = platform.uname()
+print(f"\n{uname.system} {uname.machine} (kernel={uname.release})")
+print("Physical cores:", psutil.cpu_count(logical=False))
+print("Total cores:", psutil.cpu_count(logical=True))
+cpufreq = psutil.cpu_freq()
+print(f"Max Frequency: {cpufreq.max:.2f}Mhz")
+disk = GetDiskInfo()
+print(f"Disk: {disk[0]} ({disk[1]}, {disk[2]})")
+print()
+print(tabulate(table, headers=["Method", "Time [s]"]))

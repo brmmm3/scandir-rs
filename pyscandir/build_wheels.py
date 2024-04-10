51aa883342f4c04e4dc751a98aeb25b5dd32ab2a
Martin Bammer <mrbm74@gmail.com>
2024-04-10 08:09:06 +0200

    Improve build_wheels.py

--- pyscandir/build_wheels.py
+++ pyscandir/build_wheels.py
@@ -7,7 +7,7 @@ from typing import List
 
 def Run(args: List[str]) -> subprocess.CompletedProcess:
     print("RUN:", " ".join(args))
-    if os.name == 'nt':
+    if os.name == "nt":
         return subprocess.run(args, shell=True, capture_output=True)
     return subprocess.run(" ".join(args), shell=True, capture_output=True)
 
@@ -23,7 +23,9 @@ def ShowResult(title: str, prc: subprocess.CompletedProcess):
     return stdout, prc.returncode
 
 
-def BuildWheel(versions_dir: str, version: str, python_exe: str, features: str, bDebug: bool) -> int:
+def BuildWheel(
+    versions_dir: str, version: str, python_exe: str, features: str, bDebug: bool
+) -> int:
     print(f"Building wheel for Python version {version}...")
     python_path = f"{versions_dir}/{version}/{python_exe}"
     cmd = ["maturin", "build", "--strip", "-i", python_path]
@@ -35,7 +37,9 @@ def BuildWheel(versions_dir: str, version: str, python_exe: str, features: str, 
     stdout, returncode = ShowResult("maturin build", maturin_build)
     if returncode != 0:
         return returncode
-    builtWheel = [line for line in stdout.splitlines() if "Built wheel for CPython" in line]
+    builtWheel = [
+        line for line in stdout.splitlines() if "Built wheel for CPython" in line
+    ]
     if not builtWheel:
         print("No wheel built!")
         print(stdout)
@@ -53,7 +57,8 @@ def BuildWheel(versions_dir: str, version: str, python_exe: str, features: str, 
         return returncode
 
     install_wheel = Run(
-        [python_path, "-m", "pip", "install", "--force-reinstall", wheel_path])
+        [python_path, "-m", "pip", "install", "--force-reinstall", wheel_path]
+    )
     stdout, returncode = ShowResult("install wheel", install_wheel)
     if returncode != 0:
         return returncode
@@ -67,17 +72,19 @@ def BuildWheel(versions_dir: str, version: str, python_exe: str, features: str, 
 
 
 if __name__ == "__main__":
-    versions = None
+    versions = [sys.version.split()[0]]
     if "--versions" in sys.argv:
         versions = sys.argv[sys.argv.index("--versions") + 1].split(",")
-    elif versions is None:
+    if versions == ["*"]:
         pyenv_versions = Run(["pyenv", "versions"])
         stdout, returncode = ShowResult("pyenv versions", pyenv_versions)
         if pyenv_versions.returncode != 0:
             sys.exit(1)
-        versions = [version.lstrip("*").strip().split()[0]
-                    for version in stdout.splitlines()
-                    if "system" not in version and " 2.7." not in version]
+        versions = [
+            version.lstrip("*").strip().split()[0]
+            for version in stdout.splitlines()
+            if "system" not in version and " 2.7." not in version
+        ]
     if not versions:
         print("No versions to build Python wheel!")
         sys.exit(1)
@@ -91,23 +98,25 @@ if __name__ == "__main__":
 
     python_path = Run(["pyenv", "which", "python"])
     if python_path.returncode != 0:
-        print(
-            f"'pyenv which python' failed with error code {python_path.returncode}")
+        print(f"'pyenv which python' failed with error code {python_path.returncode}")
         print(python_path.stderr.decode("utf-8"))
         sys.exit(1)
 
-    versions_dir = python_path.stdout.decode(
-        "utf-8").rsplit("versions", 1)[0] + "versions"
+    versions_dir = (
+        python_path.stdout.decode("utf-8").rsplit("versions", 1)[0] + "versions"
+    )
 
-    python_exe = "python.exe" if os.name == 'nt' else "bin/python"
+    python_exe = "python.exe" if os.name == "nt" else "bin/python"
 
     futures = {}
     with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
         for version in versions:
             futures[version] = executor.submit(
-                BuildWheel, versions_dir, version, python_exe, features, bDebug)
+                BuildWheel, versions_dir, version, python_exe, features, bDebug
+            )
         for version, future in futures.items():
             returncode = future.result()
             if returncode != 0:
                 print(
-                    f"Building wheel for Python version {version} failed with error code {returncode}!")
+                    f"Building wheel for Python version {version} failed with error code {returncode}!"
+                )
